2886594965656e70fb3df54d3ba05e0c49e79f31
twong <twong@reliaslearning.com>
2021-01-04 18:04:16 -0500

    removing firebase credential stuff

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAX9CgsEGfkw97I--Bp_bx3BpkYfZwh6Ms",
-  authDomain: "udacityfaqgenerator.firebaseapp.com",
-  databaseURL: "https://udacityfaqgenerator-default-rtdb.firebaseio.com",
-  projectId: "udacityfaqgenerator",
-  storageBucket: "udacityfaqgenerator.appspot.com",
-  messagingSenderId: "905790759617",
-  appId: "1:905790759617:web:3a27c5bd48751d26c0ea40",
-  measurementId: "G-P32NZ7L9BR"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 export const auth = firebaseApp.auth();
